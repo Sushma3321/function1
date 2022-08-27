551f4b28908e980e9c12b34d227c119d0597ce88
Your Name <you@example.com>
2022-08-27 13:49:58 +0530

    first commit

---
+++ aver sum function.py
@@ -0,0 +1,11 @@
+
+
+# Create a function which takes 3 arguments(all integers) and prints their sum and average as shown below.
+
+
+def add_average_numbers(x,y,z):
+    a=x+y+z
+    b=a//3
+    print(a)
+    print(b)
+add_average_numbers(3,4,5)
---
+++ function que.py
@@ -0,0 +1,30 @@
+# def my_function(list):
+#     i=0
+#     b=[]
+#     while i<len(list):
+#         a=[list[i]]
+#         b.append(a)
+#         i=i+1
+#     print(b)
+# my_function([1,2,3,4,5,6])
+
+
+# def my_function(list):
+
+
+
+
+
+
+i=0
+while i<=5:
+    j=1
+    while j<5:
+        print(" ",end="")
+        j=j+1
+    j=1
+    while j<=i:
+        print(i,end="")
+        j=j+1
+    print()
+    i=i+1
